d559c91d8f0f5b8eb877905d800128d0a0c3354d
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-10-23 16:04:44 +0300

    homework 1 code

---
+++ homework/homework 1/homework1.ino
@@ -0,0 +1,62 @@
+const int redValue = 0;
+const int greenValue = 1;
+const int blueValue = 2;
+
+const int potRedPin = A0;
+const int ledRedPin = 6;
+
+const int potGreenPin = A2;
+const int ledGreenPin = 5;
+
+const int potBluePin = A5;
+const int ledBluePin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(potRedPin, INPUT);
+  pinMode(potGreenPin, INPUT);
+  pinMode(potBluePin, INPUT);
+  
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(ledBluePin, OUTPUT);
+
+  
+  Serial.begin(9600);
+}
+
+int readPot(int which) {
+  if (which == redValue) {
+    return analogRead(potRedPin);
+  }
+  else if (which == greenValue) {
+    return analogRead(potGreenPin);
+  }
+  else {
+    return analogRead(potBluePin);
+  }
+}
+
+void writeLed(int which, int analogValue) {
+  int convertedValue = map(analogValue, 0, 1023, 0, 255); // analogValue is the raw given by the potentiometer
+  if(which == redValue) {
+    analogWrite(ledRedPin, convertedValue); // maybe lower red values by defauly? red is way too strong, compared to the others
+  }
+  else if (which == greenValue) {
+    analogWrite(ledGreenPin, convertedValue);
+  }
+  else {
+    analogWrite(ledBluePin, convertedValue);
+  }
+}
+
+void handleColor(int which) {
+    writeLed(which, readPot(which));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    handleColor(redValue);
+    handleColor(greenValue);
+    handleColor(blueValue);
+}
