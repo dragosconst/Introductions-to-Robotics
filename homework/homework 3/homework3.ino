5e4798b3256c9382535880611250f4dde6e5ec06
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-11-08 18:01:45 +0200

    homework 3 code

---
+++ homework/homework 3/homework3.ino
@@ -0,0 +1,102 @@
+const int sampleSize = 500;
+int antennaInputs[sampleSize];
+
+
+const int pinBuzz = 10;
+
+int baseFrequency = 500; // frequency for a detection of 5
+long lastBuzz = 0;
+long buzzStep = 100; // the step by which it gets faster, for every step above 0 on the display
+long normalInterval = 1000; // the interval for buzzes at a value of 0 on the 7-segment display
+long playInterval = 20; // how long it should play the sound
+bool screechState = LOW;
+
+int freqStep = 50; // step it adds\decreases depending on how far from 5 it is
+
+
+const int pinGhost = A0;
+
+const int pinA = 2;
+const int pinB = 3;
+const int pinC = 4;
+const int pinD = 5;
+const int pinE = 6;
+const int pinF = 7;
+const int pinG = 8;
+const int pinDP = 9;
+
+const int segSize = 8;
+const int noOfDigits = 10;
+bool dpState = LOW;
+bool state = HIGH;
+
+
+bool digitMatrix[noOfDigits][segSize - 1] = {
+// a  b  c  d  e  f  g
+  {1, 1, 1, 1, 1, 1, 0}, // 0
+  {0, 1, 1, 0, 0, 0, 0}, // 1
+  {1, 1, 0, 1, 1, 0, 1}, // 2
+  {1, 1, 1, 1, 0, 0, 1}, // 3
+  {0, 1, 1, 0, 0, 1, 1}, // 4
+  {1, 0, 1, 1, 0, 1, 1}, // 5
+  {1, 0, 1, 1, 1, 1, 1}, // 6
+  {1, 1, 1, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1}, // 8
+  {1, 1, 1, 1, 0, 1, 1}  // 9
+};
+int segments[segSize] = {
+  pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP
+};
+
+void displayNumber(byte digit, bool decimalPoint) {
+  for(int i = 0; i < segSize - 1; i++) {
+    digitalWrite(segments[i], digitMatrix[digit][i]);
+  }
+  digitalWrite(pinDP, decimalPoint);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0; i < 8; ++i) {
+    pinMode(segments[i], OUTPUT);
+  }
+  pinMode(pinBuzz, OUTPUT);
+  pinMode(pinGhost, INPUT);
+  
+
+  Serial.begin(9600);
+}
+
+void playAtIntervals(int detection) {
+  if(!screechState && millis() - lastBuzz >= normalInterval - detection * buzzStep) {
+    lastBuzz = millis();
+    screechState = HIGH;
+    tone(pinBuzz, baseFrequency + detection * freqStep);
+  }
+  else if(screechState && millis() - lastBuzz < playInterval){
+    tone(pinBuzz, baseFrequency + detection * freqStep);
+  }
+  else if(screechState && millis() - lastBuzz >= playInterval) {
+    noTone(pinBuzz);
+    lastBuzz = millis();
+    screechState = LOW;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long inputsSummed = 0;
+  for(int i = 0; i < sampleSize; ++i) {
+    antennaInputs[i] = analogRead(pinGhost);
+    inputsSummed += antennaInputs[i];
+  }
+
+  int averageInput = inputsSummed / sampleSize;
+  averageInput = constrain(averageInput, 0, 100);
+  Serial.println(averageInput);
+  averageInput = map(averageInput, 0, 100, 0, 9);
+
+  playAtIntervals(averageInput);
+  
+  displayNumber(averageInput, false);
+}
